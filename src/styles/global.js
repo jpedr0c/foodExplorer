195547802bedc1b818107cd1708d8489b879b410
jpedr0c <jpccarvalho2210@gmail.com>
2023-05-30 21:44:50 -0300

    feat: change px for rem

--- src/styles/global.js
+++ src/styles/global.js
@@ -5,17 +5,31 @@ export default createGlobalStyle`
     margin: 0;
     padding: 0;
     box-sizing: border-box;
-    /* font-family: 'Roboto Slab', serif; */
+  }
+
+  :root {
+    font-size: 62.5%;
   }
 
   body{
     background: ${({ theme }) => theme.COLORS.DARK_400};
     color: ${({ theme }) => theme.COLORS.WHITE};
+    overflow: auto;
     -webkit-font-smoothing: antialiased;
+
+      ::-webkit-scrollbar {
+          background-color: none;
+          width: 0.7rem;
+      }
+      
+      ::-webkit-scrollbar-thumb {
+          background-color: ${({ theme }) => theme.COLORS.LIGHT_700};
+          border-radius: 1rem;
+      }
   }
 
   input, button, textarea{
-    font-size: 16px;
+    font-size: 1.6rem;
     outline: none;
   }
 
