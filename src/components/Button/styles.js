13784d481382636dd382f690521b5902af0e965f
jpedr0c <jpccarvalho2210@gmail.com>
2023-05-30 23:03:49 -0300

    feat: change font and units

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -2,12 +2,14 @@ import styled from 'styled-components';
 
 export const Container = styled.button`
   width: 100%;
-  height: 56px;
-  line-height: 24px;
+  height: 5.6rem;
+  line-height: 2.4rem;
   background: ${({ theme }) => theme.COLORS.TOMATO_100};
   color: ${({ theme }) => theme.COLORS.LIGHT_100};
   border: 0;
-  border-radius: 5px;
+  border-radius: 0.5rem;
+  font-family: 'Poppins', sans-serif;
+  font-weight: 500;
 
   &:disabled {
     opacity: 0.5;
