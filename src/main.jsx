fa5d6b5ed375c532ed7fffa71bc920e86e557379
jpedr0c <jpccarvalho2210@gmail.com>
2023-05-31 22:27:24 -0300

    feat: create dish page

--- src/main.jsx
+++ src/main.jsx
@@ -5,12 +5,13 @@ import GlobalStyles from './styles/global';
 import theme from './styles/theme.js';
 import SignIn from './pages/SignIn/index.jsx';
 import SignUp from './pages/SignUp/index.jsx';
+import Dish from './pages/Dish/index.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <SignUp />
+      <Dish />
     </ThemeProvider>
   </React.StrictMode>,
 )
