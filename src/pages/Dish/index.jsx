1cf983e34bc25d876ea0dae519a6ab51a0f37118
jpedr0c <jpccarvalho2210@gmail.com>
2023-06-01 12:07:34 -0300

    feat: creating Dish page

--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -1,26 +1,14 @@
 import { Container } from './styles';
-import { Search } from '../../components/Search';
-import { Button } from '../../components/Button';
-import { IoReceipt } from 'react-icons/io5';
-import { BiSearch, BiExit } from 'react-icons/bi';
+import { Header } from '../../components/Header';
+import { Footer } from '../../components/Footer';
+import { InfoDish } from '../../components/InfoDish';
 
 export default function Dish(){
   return (
-     <Container>
-      <img src="../../../src/assets/logo.png" alt="Logo do Food Explorer" />
-      <Search
-        icon={BiSearch}
-        id="search"
-        type="text"
-        placeholder="Busque por pratos ou ingredientes"
-      />
-      <a href="">Meus favoritos</a>
-      <a href="">Histórico de pedidos</a>
-      <Button 
-        icon={IoReceipt}
-        title="Pedidos (0)"
-      />
-      <BiExit />
-     </Container>
+    <Container>
+      <Header />
+      <InfoDish></InfoDish>
+      <Footer />
+    </Container>
   )
 }
--- src/pages/Dish/styles.js
+++ src/pages/Dish/styles.js
@@ -1,30 +1,9 @@
 import styled from 'styled-components';
 
-export const Container = styled.nav`
+export const Container = styled.div`
   width: 100%;
-  max-height: 10.4rem;
-  top: 0;
-  left: 0;
+  height: 100vh;
   display: flex;
-  align-items: center;
-  background-color: ${({ theme }) => theme.COLORS.DARK_600};
-  padding: 2.4rem 12.3rem;
-  gap: 3.2rem;
-
-  > img {
-    max-width: 18.6rem;
-    max-height: 3rem;
-  }
-
-  > a {
-    display: flex;
-    font-weight: 500;
-    font-size: 1.4rem;
-    line-height: 2.4rem;
-    color: ${({ theme }) => theme.COLORS.LIGHT_400};
-  }
-
-  > svg {
-    font-size: 18rem;
-  }
+  flex-direction: column;
+  background-color: ${({ theme }) => theme.COLORS.DARK_900};
 `;
