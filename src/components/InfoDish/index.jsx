78a8aa36de33ec6a3f661a02dfae2b108705c865
jpedr0c <jpccarvalho2210@gmail.com>
2023-06-01 12:12:24 -0300

    feat: creating page with information about dishes

---
+++ src/components/InfoDish/index.jsx
@@ -0,0 +1,9 @@
+import { Container } from './styles';
+
+export function InfoDish(){
+  return (
+    <Container>
+      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptas quisquam sint quibusdam repellendus obcaecati cumque aspernatur consectetur doloribus alias magni, veritatis voluptate et libero velit ipsum fugiat, veniam eos reiciendis.</p>
+    </Container>
+  )
+}
