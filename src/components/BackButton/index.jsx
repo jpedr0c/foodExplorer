a7f9a7a72e099e695dd575359b975f384a0146ae
jpedr0c <jpccarvalho2210@gmail.com>
2023-06-01 12:05:53 -0300

    feat: creating BackButton component

---
+++ src/components/BackButton/index.jsx
@@ -0,0 +1,10 @@
+import { Container } from './styles';
+
+export function BackButton(){
+  return (
+    <Container>
+      
+      <a href="/">voltar</a>
+    </Container>
+  )
+}
---
+++ src/components/BackButton/styles.js
@@ -0,0 +1,6 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+  max-width: 10.6rem;
+  max-height: 3.4rem;
+`;
