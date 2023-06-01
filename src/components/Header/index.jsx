28b2ba35afec471dca771f4864a240dc0cffcc39
jpedr0c <jpccarvalho2210@gmail.com>
2023-06-01 12:08:38 -0300

    fix: making it more semantic

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,30 @@
+import { Container, Logout } from './styles';
+import { Search } from '../../components/Search';
+import { Button } from '../../components/Button';
+import { IoReceipt } from 'react-icons/io5';
+import { BiSearch, BiExit } from 'react-icons/bi';
+
+export function Header(){
+  return (
+    <Container>
+      <a href=""><img src="../../../src/assets/logo.png" alt="Logo do Food Explorer" /></a>
+      <Search
+        icon={BiSearch}
+        id="search"
+        type="text"
+        placeholder="Busque por pratos ou ingredientes"
+      />
+
+      {/* TODO: Colocar esses dois links para a milha extra */}
+      {/* <a href="">Meus favoritos</a>
+      <a href="">Histórico de pedidos</a> */}
+      <Button 
+        icon={IoReceipt}
+        title="Pedidos (0)"
+      />
+      <Logout>
+        <BiExit />
+      </Logout>
+    </Container>
+  )
+}
---
+++ src/components/Header/styles.js
@@ -0,0 +1,38 @@
+import styled from 'styled-components';
+
+export const Container = styled.header`
+  width: 100%;
+  max-height: 7.5rem;
+  top: 0;
+  left: 0;
+  display: flex;
+  align-items: center;
+  background-color: ${({ theme }) => theme.COLORS.DARK_600};
+  padding: 2.4rem 12.3rem;
+  gap: 3.2rem;
+
+  > a > img {
+    max-width: 18.6rem;
+    max-height: 3rem;
+    cursor: pointer;
+  }
+
+  > a {
+    display: flex;
+    font-weight: 500;
+    font-size: 1.4rem;
+    line-height: 2.4rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+  }
+`;
+
+export const Logout = styled.button`
+  border: none;
+  background: none;
+
+  > svg {
+    font-size: 3.5rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    cursor: pointer;
+  }
+`;
