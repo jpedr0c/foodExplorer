5840a27d09fa69a15729234804ab07ba778dce91
jpedr0c <jpccarvalho2210@gmail.com>
2023-06-01 22:20:33 -0300

    feat: preparing component to be dynamic

--- src/pages/Dish/index.jsx
+++ src/pages/Dish/index.jsx
@@ -2,12 +2,16 @@ import { Container } from './styles';
 import { Header } from '../../components/Header';
 import { Footer } from '../../components/Footer';
 import { InfoDish } from '../../components/InfoDish';
+// import { BackButton } from '../../components/BackButton';
 
 export default function Dish(){
   return (
     <Container>
       <Header />
-      <InfoDish></InfoDish>
+      <InfoDish data={{
+        title: "Salada Ravanello",
+        info: "Rabanetes, folhas verdes e molho agridoce salpicados com gergelim. O pao naan dá um toque especial.",
+      }}/>
       <Footer />
     </Container>
   )
