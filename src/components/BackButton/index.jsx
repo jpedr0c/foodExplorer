529a41ffdf3e4e71a87214cdcaf5adb1ff016ec3
jpedr0c <jpccarvalho2210@gmail.com>
2023-06-01 22:16:05 -0300

    feat: created BackButton component

--- src/components/BackButton/index.jsx
+++ src/components/BackButton/index.jsx
@@ -1,9 +1,10 @@
 import { Container } from './styles';
+import { AiOutlineLeft } from 'react-icons/ai';
 
 export function BackButton(){
   return (
     <Container>
-      
+      <AiOutlineLeft />
       <a href="/">voltar</a>
     </Container>
   )
--- src/components/BackButton/styles.js
+++ src/components/BackButton/styles.js
@@ -1,6 +1,24 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  max-width: 10.6rem;
+  width: 100%;
+  /* max-width: 10.6rem; */
   max-height: 3.4rem;
+  display: flex;
+  align-items: center;
+  background-color: ${({ theme }) => theme.COLORS.DARK_400};
+
+  > svg {
+    cursor: pointer;
+    font-size: 2.4rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_200};
+  }
+
+  > a {
+    font-family: 'Poppins';
+    font-weight: 700;
+    font-size: 1.8rem;
+    line-height: 3.4rem;
+    color: ${({ theme }) => theme.COLORS.LIGHT_200};
+  }
 `;
