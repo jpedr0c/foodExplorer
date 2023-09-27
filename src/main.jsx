bb73e7d2a96cccbfd29faa929563a4730ef45961
jpedr0c <jpccarvalho2210@gmail.com>
2023-09-27 19:30:39 -0300

    feat: starter configs for app

---
+++ src/main.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GlobalStyles from './styles/global';
+import { ThemeProvider } from 'styled-components';
+import theme from './styles/theme';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles/>
+      <h1>Hello world</h1>
+    </ThemeProvider>
+  </React.StrictMode>,
+)
