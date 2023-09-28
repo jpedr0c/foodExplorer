6d56c05b408f72186e067a6203cd40f10ea94a29
jpedr0c <jpccarvalho2210@gmail.com>
2023-09-28 17:26:24 -0300

    feat: creating Details page

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom/client';
 import GlobalStyles from './styles/global';
 import { ThemeProvider } from 'styled-components';
 import theme from './styles/theme';
+import { Details } from './pages/Details';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles/>
-      <h1>Hello world</h1>
+      <Details/>
     </ThemeProvider>
   </React.StrictMode>,
 )
