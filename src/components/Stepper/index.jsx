53603e7911c209f76fd8175a73202bda548f30cd
jpedr0c <jpccarvalho2210@gmail.com>
2023-10-10 19:18:26 -0300

    style: fix bugs for style

--- src/components/Stepper/index.jsx
+++ src/components/Stepper/index.jsx
@@ -1,12 +1,31 @@
-import { FiPlus, FiMinus } from "react-icons/fi"
-import { Container } from "./styles";
+import React, { useState } from "react";
+import { Plus, Minus } from "@phosphor-icons/react";
+import { Container, BtnCount } from "./styles";
 
-export function Stepper() {
+export function Stepper({ isAdmin = false }) {
+  const [count, setCount] = useState(1); // Inicializa com 1 como valor padrão
+
+  const handleIncrement = () => {
+    setCount((prevCount) => prevCount + 1); // Incrementa o valor atual
+  };
+
+  const handleDecrement = () => {
+    if (count > 1) {
+      // Verifica se o valor atual é maior que 1 antes de decrementar
+      setCount((prevCount) => prevCount - 1); // Decrementa o valor atual
+    }
+  };
+
   return(
-    <Container>
-      <button id="minus"><FiMinus/></button>
-      <span>01</span>
-      <button id="plus"><FiPlus/></button>
-    </Container>
+    !isAdmin && 
+      <Container>
+      <BtnCount onClick={handleDecrement}>
+        <Minus/>
+      </BtnCount>
+      <span>{count.toString().padStart(2, "0")}</span>
+      <BtnCount onClick={handleIncrement}>
+        <Plus/>
+      </BtnCount>
+    </Container>  
   );
 }
