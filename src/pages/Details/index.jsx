b138b66a4b50cd0af999c747cc97d4c26da108fa
jpedr0c <jpccarvalho2210@gmail.com>
2023-10-10 19:19:47 -0300

    feat: fix bugs

--- src/pages/Details/index.jsx
+++ src/pages/Details/index.jsx
@@ -1,36 +1,55 @@
-import { PiReceipt } from "react-icons/pi"
-import { Container, Plate, Info, Actions } from './styles';
+import { useState } from "react";
+import { Receipt } from "@phosphor-icons/react";
+import { Container, Box, Plate, Info, Actions } from './styles';
+import { MenuMobal } from "../../components/MenuMobal";
 import { Header } from '../../components/Header';
-import { Footer } from '../../components/Footer';
-import { Button } from '../../components/Button';
-import { Stepper } from '../../components/Stepper';
-import { Tags } from '../../components/Tags';
 import { BackButton } from '../../components/BackButton';
+import { Tags } from '../../components/Tags';
+import { Stepper } from '../../components/Stepper';
+import { Button } from '../../components/Button';
+import { Footer } from '../../components/Footer';
 
-export function Details() {
+
+export function Details({ isAdmin = false }) {
+  const [menuIsOpen, setMenuIsOpen] = useState(false);
+
   return(
     <Container>
-      <Header/>
-      <BackButton/>
-      <Plate>
-        <img src="../../../src/assets/salada_ravanello.png" alt="" />
-        <Info>
-          <h2>Salada Ravanello</h2>
-          <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.</p>
-          <Tags data={[
-            {id: "1", name: "alface"}, 
-            {id: "2", name: "cebola"}, 
-            {id: "3", name: "pão naan"}, 
-            {id: "4", name: "pepino"}, 
-            {id: "5", name: "rabanete"},
-            {id: "6", name: "tomate"},
-          ]}/>
-          <Actions>
-            <Stepper/>
-            <Button title="pedir ∙ R$ 25,00" icon={PiReceipt}/>
-          </Actions>
-        </Info>
-      </Plate>
+      <MenuMobal
+        isAdmin={isAdmin}
+        menuIsOpen={menuIsOpen}
+        onCloseMenu={() => setMenuIsOpen(false)}
+      />
+      <Header isAdmin={isAdmin} onOpenMenu={() => setMenuIsOpen(true)}/>
+      <Box>
+        <BackButton/>
+        <Plate>
+          <img src="../../../src/assets/salada_ravanello.png" alt="" />
+          <Info>
+            <h2>Salada Ravanello</h2>
+            <p>Rabanetes, folhas verdes e molho agridoce salpicados com gergelim.</p>
+            <Tags data={[
+              {id: "1", name: "alface"}, 
+              {id: "2", name: "cebola"}, 
+              {id: "3", name: "pão naan"}, 
+              {id: "4", name: "pepino"}, 
+              {id: "5", name: "rabanete"},
+              {id: "6", name: "tomate"},
+            ]}/>
+
+            {isAdmin ? 
+              <Actions isAdmin={isAdmin}>
+                <Button title={isAdmin ? "Editar prato" : "pedir ∙ R$ 25,00"} icon={Receipt}/>
+              </Actions>
+              :
+              <Actions isAdmin={isAdmin}>
+                <Stepper/>
+                <Button title={isAdmin ? "Editar prato" : "pedir ∙ R$ 25,00"} icon={Receipt}/>
+              </Actions>
+            }
+          </Info>
+        </Plate>
+      </Box>
       <Footer/>
     </Container>
   );
--- src/pages/Details/styles.js
+++ src/pages/Details/styles.js
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints';
 
 export const Container = styled.div`
   display: flex;
@@ -8,11 +9,14 @@ export const Container = styled.div`
   width: 100%;
   height: 100dvh;
   overflow-x: hidden;
-  /* @media (min-width: 768px){
-    &{
-      background-color: red;
-    }
-  } */
+`;
+
+export const Box = styled.div`
+  width: 100%;
+  max-width: 1200px;
+  display: flex;
+  flex-direction: column;
+  padding: 3rem;
 `;
 
 export const Plate = styled.main`
@@ -20,12 +24,32 @@ export const Plate = styled.main`
   flex-direction: column;
   align-items: center;
   justify-content: center;
+  text-align: center;
   width: 100%;
-  padding: 0 8.5rem;
+  margin-top: 2rem;
 
   > img {
     width: 26.4rem;
     height: 26.4rem;
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}){
+    flex-direction: row;
+    gap: 5rem;
+    margin-top: 4.2rem;
+    margin-bottom: 9rem;
+
+    > img {
+      width: 33rem;
+      height: 33rem;
+    }
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.XL}){
+    > img {
+      width: 38rem;
+      height: 38rem;
+    }
   }
 `;
 
@@ -48,12 +72,32 @@ export const Info = styled.div`
     text-align: center;
     margin: 2.4rem 0;
   }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}){
+    align-items: start;
+
+    > h2 {
+      font-size: 4rem;
+    }
+
+    > p {
+      text-align: start;
+    }
+
+    > div {
+      justify-content: start;
+    }
+  }
 `;
 
 export const Actions = styled.div`
   display: flex;
   align-items: center;
-  justify-content: space-between;
+  gap: 1.6rem;
   width: 100%;
   margin-top: 4.8rem;
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
+    width: ${({ isAdmin }) => isAdmin ? "40%" : "100%"};
+  }
 `;
