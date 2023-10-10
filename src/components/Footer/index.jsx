1bf7f036a8a0f681bfc2749e912a53c6851a6e6f
jpedr0c <jpccarvalho2210@gmail.com>
2023-10-10 19:13:05 -0300

    feat: add limit width

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -1,13 +1,15 @@
-import { Container, Logo } from "./styles";
+import { Container, Box, Logo } from "./styles";
 
 export function Footer() {
   return(
     <Container>
-      <Logo href="/">
-          <img src="../../../src/assets/polygon_gray.svg" alt="" />
-          <h1>food explorer</h1>
-      </Logo>
-      <span>© 2023 - Todos os direitos reservados.</span>
+      <Box>
+        <Logo href="/">
+            <img src="../../../src/assets/polygon_gray.svg" alt="" />
+            <h1>food explorer</h1>
+        </Logo>
+        <span>© 2023 - Todos os direitos reservados.</span>
+      </Box>
     </Container>
   );
 }
--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,18 +1,31 @@
 import styled from 'styled-components';
+import { DEVICE_BREAKPOINTS } from '../../styles/deviceBreakpoints';
 
 export const Container = styled.footer`
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
   width: 100%;
   padding: 2.4rem;
   margin-top: 3.2rem;
   background-color: ${({ theme }) => theme.COLORS.DARK_600};
+`;
+
+export const Box = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  width: 100%;
+  max-width: 1200px;
+  margin: 0 auto;
 
   > span {
     color: ${({ theme }) => theme.COLORS.LIGHT_200};
     font-family: "Poppins", sans-serif;
-    font-size: 1.2rem;
+    font-size: 1rem;
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.MD}){
+    > span {
+      font-size: 1.2rem;
+    }
   }
 `;
 
