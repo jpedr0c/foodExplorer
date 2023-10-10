6412d726baaca4b106d464aad8149bd1b6c32eb4
jpedr0c <jpccarvalho2210@gmail.com>
2023-10-10 19:20:05 -0300

    feat: fix bug on the button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,9 +1,10 @@
 import { Container } from "./styles";
 
-export function Button({ icon: Icon, title }) {
+export function Button({ icon: Icon, title, ...rest }) {
   return(
     <Container
       type="button"
+      {...rest}
     >
       {Icon && <Icon size={20}/>}
       {title}
