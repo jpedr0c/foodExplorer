b2a8b1d362667a6010857a7042a292ddf1f0b582
jpedr0c <jpccarvalho2210@gmail.com>
2023-10-10 19:16:00 -0300

    feat: fix bugs

--- src/components/PlateCard/index.jsx
+++ src/components/PlateCard/index.jsx
@@ -1,24 +1,41 @@
-import { AiFillHeart, AiOutlineHeart } from "react-icons/ai";
-import { PiPencilSimple } from "react-icons/pi"
-import { Container, Actions } from "./styles";
+import React, { useState } from "react";
+import { AiFillHeart, AiOutlineHeart } from "react-icons/ai"
+import { Pencil } from "@phosphor-icons/react";
+import { Container, ButtonTop, Actions } from "./styles";
 import { Button } from "../Button";
 import { Stepper } from "../Stepper";
 
-export function PlateCard({ data, isFavorite, isAdmin = false }) {
-  return(
-    <Container>
-      { isAdmin ? (<PiPencilSimple/>) : (isFavorite ? <AiFillHeart/> : <AiOutlineHeart/>) }
+export function PlateCard({ data, isAdmin = false }) {
+  const [isFavorite, setIsFavorite] = useState(false);
+
+  const toggleFavorite = () => {
+    setIsFavorite(!isFavorite);
+  };
+
+  data.isFavorite = toggleFavorite;
+
+  return (
+    <Container
+      data-is-admin={isAdmin}
+    >
+      {isAdmin ? (
+        <ButtonTop>
+          <Pencil />
+        </ButtonTop>
+      ) : (
+        <ButtonTop onClick={toggleFavorite}>
+          {isFavorite ? <AiFillHeart className="colored"/> : <AiOutlineHeart/>}
+        </ButtonTop>
+      )}
       <img src={data.image} alt="" />
       <h4>{data.title}</h4>
+      <span>{data.description}</span>
       <p>R$ {data.price}</p>
 
-      {
-        !isAdmin &&
-        <Actions>
-          <Stepper/>
-          <Button title="incluir"/>
-        </Actions>
-      }
+      <Actions>
+        <Stepper />
+        <Button title="incluir" />
+      </Actions>
     </Container>
   );
 }
--- src/components/PlateCard/styles.js
+++ src/components/PlateCard/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.div`
   position: relative;
@@ -14,14 +15,6 @@ export const Container = styled.div`
   background-color: ${({ theme }) => theme.COLORS.DARK_200};
   border-radius: 1rem;
 
-  > svg {
-    position: absolute;
-    top: 1.6rem;
-    right: 1.6rem;
-    font-size: 2.4rem;
-    cursor: pointer;
-  }
-
   > img {
     width: 10rem;
     height: 10rem;
@@ -36,11 +29,65 @@ export const Container = styled.div`
     text-align: center;
   }
 
+  > span {
+    display: none;
+    color: ${({ theme }) => theme.COLORS.LIGHT_400};
+    text-align: center;
+    font-family: "Roboto", sans-serif;
+    font-size: 1.2rem;
+    line-height: 2.2rem;
+  }
+
   > p {
     font-family: "Roboto", sans-serif;
     font-size: 1.6rem;
     color: ${({ theme }) => theme.COLORS.CAKE_200};
   }
+
+  &[data-is-admin="true"] {
+    max-height: 30rem;
+
+    > div{
+      display: none;
+    }
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.MD}){
+    > span {
+      display: block;
+    }
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}){
+    > img {
+      width: 12rem;
+      height: 12rem;
+    }
+
+    > h4 {
+      font-size: 1.8rem;
+    }
+
+    > p {
+      font-size: 2rem;
+    }
+  }
+`;
+
+export const ButtonTop = styled.button`
+  background-color: transparent;
+  border: none;
+  color: ${({ theme }) => theme.COLORS.LIGHT_300};
+  position: absolute;
+  top: 1.6rem;
+  right: 1.6rem;
+  font-size: 2.4rem;
+  cursor: pointer;
+  
+  > svg.colored {
+    color: ${({ theme }) => theme.COLORS.TOMATO_300};
+  }
+  
 `;
 
 export const Actions = styled.div`
