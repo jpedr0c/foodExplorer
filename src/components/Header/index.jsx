142e735179fb2e5a54a5b19d056b48b991fb1c71
jpedr0c <jpccarvalho2210@gmail.com>
2023-10-14 03:30:24 -0300

    feat: add Link for page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,18 +1,19 @@
 import { List, Receipt, SignOut } from "@phosphor-icons/react";
 import { Container, Menu, Logo, Box, BtnSignOut, Cart } from "./styles";
+import { Link } from "react-router-dom";
 import { Search } from "../Search";
 import { Button } from "../Button";
 
 export function Header({ isAdmin = false, onOpenMenu, orders = "0" }) {
   return(
     <Container
-      $isadmin={isAdmin}
+      isAdmin={isAdmin}
     >
       <Box>
         <Menu onClick={onOpenMenu}>
           <List/>
         </Menu>
-        <Logo href="/">
+        <Logo to="/">
             <img src="../../../src/assets/polygon.svg" alt="" />
             <div className="text">
               <h1>food explorer</h1>
@@ -20,7 +21,9 @@ export function Header({ isAdmin = false, onOpenMenu, orders = "0" }) {
             </div>
         </Logo>
         <Search className="desktop" placeholder="Busque por pratos ou ingredientes"/>
-        <Button className="desktop" icon={isAdmin ? "" : Receipt} title={isAdmin ? "Novo prato" : `Pedidos (${orders})`} />
+        <Link to={isAdmin ? "/new" : ""} className="desktop">
+          <Button icon={isAdmin ? "" : Receipt} title={isAdmin ? "Novo prato" : `Pedidos (${orders})`} />
+        </Link>
         <BtnSignOut className="desktop">
           <SignOut/>
         </BtnSignOut>
