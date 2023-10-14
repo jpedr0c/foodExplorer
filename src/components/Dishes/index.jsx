793dafb71989606b14e836fbaf49a220391806e1
jpedr0c <jpccarvalho2210@gmail.com>
2023-10-14 20:04:30 -0300

    feat: slide for dishes

--- src/components/Dishes/index.jsx
+++ src/components/Dishes/index.jsx
@@ -1,12 +1,41 @@
-import { Container, Carousel } from "./styles";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { FreeMode, Navigation } from "swiper/modules";
+import { Container } from "./styles";
+import { PlateCard } from "../PlateCard";
 
-export function Dishes({ title, children }) {
+import 'swiper/css';
+import 'swiper/css/free-mode';
+import 'swiper/css/navigation';
+
+export function Dishes({ isAdmin = false, title, dishes }) {
   return(
     <Container>
       <h3>{title}</h3>
-      <Carousel>
-        {children}
-      </Carousel>
+      <Swiper
+        slidesPerView="auto"
+        spaceBetween={12}
+        freeMode={true}
+        navigation={true}
+        modules={[FreeMode, Navigation]}
+        className="mySwiper"
+        >
+        { dishes &&
+          dishes.map(dish => (
+            <SwiperSlide
+              key={dish.id}
+            >
+              <PlateCard 
+                isAdmin={isAdmin} 
+                id={dish.id}
+                title={dish.title}
+                description={dish.description}
+                price={dish.price}
+                image={dish.image}
+                favorite={dish.isFavorite}
+              />
+            </SwiperSlide>
+        ))}
+      </Swiper>
     </Container>
   );
 }
--- src/components/Dishes/styles.js
+++ src/components/Dishes/styles.js
@@ -1,21 +1,68 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.section`
   width: 100%;
   margin-bottom: 5rem;
-  padding-left: 2.4rem;
+  padding-left: 1rem;
+  padding-right: 1rem;
 
+  .swiper {
+    width: 100%;
+    position: relative;
+  }
+
+  .swiper-slide{
+    width: fit-content;
+  }
+
+  .swiper-button-next {
+    justify-content: flex-end;
+    padding-right: 1rem;
+    right: 0;
+    background: linear-gradient(90deg, rgba(0,10,15,0) 0%, rgba(0,10,15,1) 70%);
+  }
+
+  .swiper-button-prev {
+    justify-content: flex-start;
+    padding-left: 1rem;
+    left: 0;
+    background: linear-gradient(90deg, rgba(0,10,15,1) 0%, rgba(0,10,15,0) 70%);
+  }
+
+  .swiper-button-next,
+  .swiper-button-prev{
+    display: none;
+    color: ${({ theme }) => theme.COLORS.LIGHT_100};
+    height: 100%;
+    width: 8%;
+    top: 2.2rem;
+
+    &::after {
+      font-size: 2.8rem;
+    }
+  }
+  
   > h3 {
     font-family: "Poppins", sans-serif;
     font-size: 1.8rem;
     font-weight: 500;
     margin-bottom: 2.4rem;
   }
-`;
+  
+  @media (min-width: ${DEVICE_BREAKPOINTS.LG}) {
+    .swiper-button-next,
+    .swiper-button-prev {
+      display: flex;
+      width: 10%;
+    }
 
-export const Carousel = styled.div`
-  width: 110%;
-  display: flex;
-  align-items: center;
-  gap: 1.6rem;
+    .swiper-button-disabled {
+      display: none;
+    }
+
+    > h3 {
+      font-size: 2.5rem;
+    }
+  }
 `;
